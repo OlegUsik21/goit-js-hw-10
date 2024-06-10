4e49004414e5baa42eda0bd53fa8aa04f062bc33
OlegUsik21 <149603285+OlegUsik21@users.noreply.github.com>
2024-06-10 10:10:08 +0000

    Deploying to gh-pages from @ OlegUsik21/goit-js-hw-10@082d2682c05ab06e3091eb9384be5f53911775bd 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i}from"./assets/vendor-77e16229.js";const s=document.querySelector(".form");s.addEventListener("submit",m=>{m.preventDefault();const t=Number(s.elements.delay.value),r=s.elements.state.value;new Promise((e,o)=>{setTimeout(()=>{r==="fulfilled"?e(`Fulfilled promise in ${t}ms`):r==="rejected"&&o(`Rejected promise in ${t}ms`)},t)}).then(e=>{i.success({message:e})}).catch(e=>{i.error({message:e})}),s.reset()});
+//# sourceMappingURL=commonHelpers2.js.map
